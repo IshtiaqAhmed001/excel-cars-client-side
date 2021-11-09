d46a5085dc19e3a2288b04077a6ea6bd0255012c
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-11-10 01:41:23 +0600

    initialized firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home/Home';
+import Cars from './components/Cars/Cars';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+          <Route path="/cars">
+            <Cars></Cars>
+          </Route>
+          <Route path="/login">
 
-      </header>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
