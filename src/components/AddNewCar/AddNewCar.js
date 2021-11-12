be2feb81e0ed94a7cfefcb2f165ad1cd08cf9dab
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-11-13 04:40:28 +0600

    new car add done

--- src/components/AddNewCar/AddNewCar.js
+++ src/components/AddNewCar/AddNewCar.js
@@ -1,9 +1,65 @@
 import React from 'react';
-
+import { useForm } from "react-hook-form";
 const AddNewCar = () => {
+    const { register, handleSubmit, formState: { errors }, reset } = useForm();
+
+    const onSubmit = data => {
+
+        const { name, price, img, coverImg, madeIn, milage, fuel, mode, seats, released } = data;
+
+        const newCar = { name, price, img, coverImg, madeIn, milage, fuel, mode, seats, released };
+
+        fetch('http://localhost:5000/cars', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCar)
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.insertedId) {
+                    alert('New Car added successfully!');
+                    reset();
+                }
+            })
+    }
+
     return (
         <div>
-            <h1>Add New Car</h1>
+            <h2 className="display-6 fw-bold mt-5">Add a New Car</h2>
+            <div className="w-75 mx-auto my-5">
+                <form onSubmit={handleSubmit(onSubmit)}>
+
+                    <input className="form-control" placeholder="Car Name "  {...register("name", { required: true })} />
+                    <br />
+                    <input className="form-control" placeholder="Made-in "  {...register("madeIn", { required: true })} />
+                    <br />
+                    <input className="form-control" placeholder="Price "  {...register("price", { required: true })} />
+                    <br />
+                    <input className="form-control" placeholder="Image URL "  {...register("img", { required: true })} />
+                    <br />
+                    <input className="form-control" placeholder="Cover Img URL "  {...register("coverImg", { required: true })} />
+                    <br />
+                    <input className="form-control" placeholder="Released"  {...register("released", { required: true })} />
+                    <br />
+                    <input className="form-control" placeholder="Driving Mode"  {...register("mode", { required: true })} />
+                    <br />
+                    <input className="form-control" placeholder="Fuel Type"  {...register("fuel", { required: true })} />
+                    <br />
+                    <input className="form-control" placeholder="Milage"  {...register("milage", { required: true })} />
+                    <br />
+                    <input className="form-control" placeholder="Seats"  {...register("seats", { required: true })} />
+                    <br />
+
+
+
+
+                    {errors.exampleRequired && <span>This field is required</span>}
+
+                    <input className="btn btn-lg btn-danger p-2 w-25" value="Add Car" type="submit" />
+                </form>
+            </div>
         </div>
     );
 };
