4e70e044d5afcbcad5f792e85bf6c374a18a2628
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-11-13 03:16:30 +0600

    completed my orders page

--- src/components/MyOrders/MyOrders.js
+++ src/components/MyOrders/MyOrders.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Table } from 'react-bootstrap';
 import useAuth from '../../hooks/useAuth';
 
 const MyOrders = () => {
@@ -12,9 +13,55 @@ const MyOrders = () => {
             .then(data => setOrders(data));
     }, [user.email]);
 
+    const handleCancelOrder = id => {
+        const proceed = window.confirm('Are you sure you want to cancel ?');
+        if (proceed) {
+            const url = `http://localhost:5000/myorders/${id}`;
+            fetch(url, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('Order canceled!');
+                        window.location.reload();
+                    }
+                })
+        }
+    }
+
     return (
         <div className="my-5">
-            <h1>My orders: {orders.length}</h1>
+            <h1 className="display-5 mt-5">Number of Orders: {orders.length}</h1>
+            <hr className="w-50 mx-auto mb-5" />
+
+            <div>
+                <Table responsive="sm w-75 mx-auto">
+                    <thead>
+                        <tr>
+                            <th className="fs-4">Car Name</th>
+                            <th className="fs-4">Price</th>
+                            <th className="fs-4">Cancel Order</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            orders.map(order => < tr >
+                                <td className="fw-bold d-flex align-items-center">
+                                    <div className="mx-2">
+                                        <img className="border border-2 border-dark" width="100px" src={order.itemDetails.img} alt="" />
+                                    </div>
+                                    <div>
+                                        {order.itemDetails.name}
+                                    </div>
+                                </td>
+                                <td className="fw-bold fs-5">{order.itemDetails.price}</td>
+                                <td><button onClick={() => handleCancelOrder(order._id)} className="btn btn-lg btn-danger text-dark  fw-bold ">Cancel</button></td>
+                            </tr>)
+                        }
+                    </tbody>
+                </Table>
+            </div>
         </div>
     );
 };
