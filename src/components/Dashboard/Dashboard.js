a94d4c30db04028ee1b22567087305a11b1f2dcd
IshtiaqAhmed001 <ishtiaq.wasi@gmail.com>
2021-11-13 22:38:17 +0600

    edited dashboard

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Spinner } from 'react-bootstrap';
 import { NavLink } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
 import useRegisteredUsers from '../../hooks/useRegisteredUsers';
 import './Dashboard.css';
+
 
 const Dashboard = () => {
     const { user, logOut } = useAuth();
@@ -14,23 +14,23 @@ const Dashboard = () => {
     const loggedInUser = registeredUser[0];
 
     return (
-        <div className="d-flex ">
-            <div className="side-bar ms-0 ">
+        <div className="d-flex">
+            <div className="side-bar ms-0 w-25 ">
                 {
                     loggedInUser?.role === 'admin' ? <ul className="my-5">
-                        <li className="my-3"><NavLink className="navLink" to="/manageallproducts">Manage All Products</NavLink>
+                        <li className="my-3"><NavLink className="sidebar-links" to="/manageallproducts">Manage All Products</NavLink>
                         </li>
-                        <li className="my-3"> <NavLink className="navLink" to="/addnewcar">Add a New Car</NavLink></li>
-                        <li className="my-3"><NavLink className="navLink" to="/manageallorders">Manage All Orders</NavLink>
+                        <li className="my-4"> <NavLink className="sidebar-links" to="/addnewcar">Add a New Car</NavLink></li>
+                        <li className="my-4"><NavLink className="sidebar-links" to="/manageallorders">Manage All Orders</NavLink>
                         </li>
-                        <li className="my-3"><NavLink className="navLink" to="/makeadmin">Make Admin</NavLink>
+                        <li className="my-4"><NavLink className="sidebar-links" to="/makeadmin">Make Admin</NavLink>
                         </li>
-                        <li className="my-3"><button className="side-bar-logout-btn" onClick={logOut}>Log Out</button></li>
+                        <li className="my-4"><button className="side-bar-logout-btn" onClick={logOut}>Log Out</button></li>
                     </ul> : <ul>
-                        <li className="my-3"><NavLink className="navLink" to="/myorders">My Orders</NavLink></li>
-                        <li className="my-3"><NavLink className="navLink" to="/addnewreview">Add Review</NavLink>
+                        <li className="my-4"><NavLink className="sidebar-links" to="/myorders">My Orders</NavLink></li>
+                        <li className="my-4"><NavLink className="sidebar-links" to="/addnewreview">Add Review</NavLink>
                         </li>
-                        <li className="my-3"><NavLink className="navLink" to="/makepayment">Make Payment</NavLink>
+                        <li className="my-4"><NavLink className="sidebar-links" to="/makepayment">Make Payment</NavLink>
                         </li>
                         <li className="my-3"><button className="side-bar-logout-btn" onClick={logOut}>Log Out</button></li>
                     </ul>
@@ -38,8 +38,8 @@ const Dashboard = () => {
 
             </div>
 
-            <div>
-                <h1>This is dashboard</h1>
+            <div className="w-75">
+                <img className="w-100" src="https://i.ibb.co/hmpFbcZ/dashboard-Bg.jpg" alt="" />
             </div>
         </div >
     );
